7a396ee69673ab12e6c02f1ac883808c066aeb22
李志金 <11563806+lizhijn@user.noreply.gitee.com>
2023-01-26 20:26:39 +0800

    login0.1

---
+++ src/components/Login.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="lg">
+    登录界面
+    <div class="lg_box">
+      <!-- 头像 -->
+      <div class="top">
+        <img src="@/assets/logo.png" alt="" />
+      </div>
+      <!-- 用户名与密码 -->
+      <el-form
+        :label-position="labelPosition"
+        :model="formLabelAlign"
+        class="bd"
+        :rules="LoginRules"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="user">
+          <el-input
+            v-model="formLabelAlign.user"
+            clearable
+            prefix-icon="el-icon-user-solid"
+            placeholder="请输入用户名"
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            placeholder="请输入密码"
+            prefix-icon=" el-icon-lock "
+            v-model="formLabelAlign.password"
+            show-password
+          ></el-input>
+        </el-form-item>
+        <!-- 登录与重置 -->
+        <el-form-item class="an">
+          <el-button type="primary" @click="submitForm('ruleForm')"
+            >登录</el-button
+          >
+          <el-button type="info" @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      labelPosition: "right",
+      formLabelAlign: {
+        user: "admin",
+        password: "123456",
+      },
+      LoginRules: {
+        user: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          {
+            min: 6,
+            max: 16,
+            message: "密码长度为在 6 到 16 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  components: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+<style scoped lang='less'>
+.lg {
+  width: 100%;
+  height: 100%;
+  background-color: rgb(40, 74, 102);
+  .lg_box {
+    width: 450px;
+    height: 300px;
+    background-color: aliceblue;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    border-radius: 10px;
+    transform: translate(-50%, -50%);
+    .top {
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+      border: 1px solid #eee;
+      box-shadow: 0 0 10px rgb(236, 238, 235);
+      padding: 10px;
+      background-color: rgba(98, 98, 89, 0.126);
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: rgb(236, 238, 235);
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: rgb(236, 238, 235);
+      }
+    }
+    .bd {
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      box-sizing: border-box;
+      padding: 0 10%;
+    }
+  }
+  .an {
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(ElementUI);
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 添加token请求头
+  let token_ = localStorage.getItem('token');
+  if (token_) {
+    config.headers['token'] = token_;
+  }
+  // 在发送请求之前做些什么
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
