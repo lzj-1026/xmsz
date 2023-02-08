fd0bbff0ce43d5eddbac826216eff72fd925b207
李志金 <11563806+lizhijn@user.noreply.gitee.com>
2023-02-08 15:29:27 +0800

    xm1.0

--- src/main.js
+++ src/main.js
@@ -6,12 +6,16 @@ import 'element-ui/lib/theme-chalk/index.css';
 import '@/assets/font/iconfont.css'
 import TreeTable from 'vue-table-with-tree-grid'
 
-
+// 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 Vue.use(VueQuillEditor);
+
+// 导入nprogress包对应的js和css
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css'
 
 // 引入echarts
 //  引入Echarts
@@ -27,11 +31,20 @@ import VueAxios from 'vue-axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.prototype.$http = axios
 // 添加请求拦截器
+
+//在request  拦截器中展示进度条NProgress.start()
 axios.interceptors.request.use(function (config) {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+
   return config
 });
 
+// 在response拦截器隐藏进度条NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
 // 注册tree-table
 
 Vue.component('tree-table', TreeTable)
